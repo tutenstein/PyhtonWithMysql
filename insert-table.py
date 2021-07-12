b993a059f79558000bbd26f09895be38ad8099f9
tutenstein <58851436+tutenstein@users.noreply.github.com>
2021-07-12 12:20:43 +0300

    Add files via upload
    Insert into mysql table from python

---
+++ insert-table.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+
+def insertProducts(list):
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "root_pass",
+        database = "node-app"
+    )
+    cursor = connection.cursor()
+    sql = 'INSERT INTO products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)'
+    values = list
+    cursor.executemany(sql,values)
+
+    try:
+        connection.commit()
+        print(f'{cursor.rowcount} registration added ')
+    except mysql.connector.Error as err:
+        print('Error: ',err)
+    finally:
+        connection.close()
+        print("Database connection closed.")
+
+list = []
+while True:
+    name = input('product Name: ')
+    price = float(input('Product price: '))
+    imageUrl = input('Product image url: ')
+    description = input('Product description: ')
+    list.append((name,price,imageUrl,description))
+    result = input("do you want to continue(y/n)? ")
+    if result == 'h':
+        print('your registrations are being added to the database...')
+        insertProducts(list)
+        break
+
+
